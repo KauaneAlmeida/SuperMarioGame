f12eaca1da80cd37d820e378db4a7a04d090ace0
Kauane Almeida <islakauane51@gmail.com>
2024-08-17 18:21:38 -0300

    Ajuste na responsividade e centralização dos elementos

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
     const backgroundMusic = document.getElementById('background-music');
     const gameOverMusic = document.getElementById('game-over-music');
+    const princess = document.querySelector('.princess');
+    const pipe = document.querySelector('.pipe');
+    const coin = document.querySelector('.coin');
     let musicStarted = false;
+    let gameStarted = false;
+    let gameLoop; 
 
     const startMusic = () => {
         if (!musicStarted) {
@@ -23,68 +28,67 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     };
 
-    document.getElementById('start-button').addEventListener('click', () => {
-        startMusic();
-        document.getElementById('start-button').style.display = 'none';
-    });
-
-    const princess = document.querySelector('.princess');
-    const pipe = document.querySelector('.pipe');
-    const coin = document.querySelector('.coin');
-
     const jump = () => {
-        princess.classList.add('jump');
-
-        setTimeout(() => {
-            princess.classList.remove('jump');
-        }, 500);
+        if (gameStarted) {
+            princess.classList.add('jump');
+            setTimeout(() => {
+                princess.classList.remove('jump');
+            }, 700);
+        }
     };
 
     const resetCoin = () => {
-        coin.style.display = 'block'; 
-        coin.style.left = `${pipe.offsetLeft + 10}px`; 
+        coin.style.display = 'block';
+        coin.style.left = `${pipe.offsetLeft + 10}px`;
         coin.style.top = `${parseInt(pipe.style.height) + 10}px`;
     };
 
-    const loop = setInterval(() => {
-        const pipePosition = pipe.offsetLeft;
-        const princessPosition = +window.getComputedStyle(princess).bottom.replace('px', '');
-        const coinPosition = coin.offsetLeft;
-        const coinHeight = +window.getComputedStyle(coin).bottom.replace('px', '');
+    const startGame = () => {
+        gameStarted = true;
+        startMusic();
+        document.getElementById('start-button').style.display = 'none';
 
-    
-        coin.style.left = `${pipePosition + 10}px`; 
-        coin.style.top = `${parseInt(pipe.style.height) + 10}px`;
+      
+        pipe.style.animation = 'pipe-animation 1.5s infinite linear';
+        coin.style.animation = 'coin-animation 1.5s infinite linear';
 
-        if (coinPosition <= 90 && coinPosition > 0 && princessPosition >= coinHeight) {
-            coin.style.display = 'none'; 
-            console.log('Moeda coletada!');
-        }
+     
+        gameLoop = setInterval(() => {
+            const pipePosition = pipe.offsetLeft;
+            const princessPosition = +window.getComputedStyle(princess).bottom.replace('px', '');
+            const coinPosition = coin.offsetLeft;
+            const coinHeight = +window.getComputedStyle(coin).bottom.replace('px', '');
 
-        if (pipePosition <= 90 && pipePosition > 0 && princessPosition < 100) {
-            pipe.style.animation = 'none';
-            pipe.style.left = `${pipePosition}px`;
+            coin.style.left = `${pipePosition + 10}px`;
+            coin.style.top = `${parseInt(pipe.style.height) + 10}px`;
 
-            princess.style.animation = 'none';
-            princess.style.bottom = `${princessPosition}px`;
+            if (coinPosition <= 100 && coinPosition > 0 && princessPosition >= coinHeight) {
+                coin.style.display = 'none';
+                console.log('Moeda coletada!');
+            }
 
-            princess.src = './imagens/gameover.png';
-            princess.style.width = '75px';
-            princess.style.marginLeft = '50px';
+            if (pipePosition <= 90 && pipePosition > 0 && princessPosition < 100) {
+                pipe.style.animation = 'none';
+                pipe.style.left = `${pipePosition}px`;
 
-            clearInterval(loop);
-            playGameOverMusic();
-        }
+                princess.style.animation = 'none';
+                princess.style.bottom = `${princessPosition}px`;
 
-        if (pipePosition <= 0) {
-            pipe.style.animation = '';
-            princess.style.animation = '';
-            resetCoin();
-        }
-    }, 10);
+                princess.src = './imagens/gameover.png';
+                princess.style.width = '75px';
+                princess.style.marginLeft = '50px';
 
+                clearInterval(gameLoop); 
+                playGameOverMusic();
+            }
+
+            if (pipePosition <= 0) {
+                resetCoin();
+            }
+        }, 10);
+    };
+
+    document.getElementById('start-button').addEventListener('click', startGame);
     document.addEventListener('keydown', jump);
     document.addEventListener('touchstart', jump);
 });
-
-
